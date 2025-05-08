2cdf5ca88f343bcf27529164271fde2e2c5a8509
Guilherme Torres <guiatorres8@gmail.com>
2025-05-08 16:53:22 -0300

    add validation and password encryption

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,11 @@
-import { Role } from '@prisma/client';
+import type { Role } from '@prisma/client';
+import bcrypt from 'bcrypt';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import type { Request, Response } from 'express';
 import express from 'express';
 import prisma from './lib/prisma';
+import { userSchema } from './validators/userSchema';
 
 dotenv.config();
 
@@ -14,47 +17,69 @@ app.use(express.json());
 
 // ------------------------------------------------------------------------------------
 
-// async function main() {
-// 	const tutor = await prisma.user.create({
-// 		data: {
-// 			name: 'Adolfo',
-// 			email: 'adolfo@exemplo.com',
-// 			password: 'senhasegura123',
-// 			role: Role.TUTOR,
-// 		},
-// 	});
-
-// 	console.log('Tutor criado:', tutor);
-
-// 	const dog = await prisma.dog.create({
-// 		data: {
-// 			name: 'Bruno',
-// 			breed: 'Maltês',
-// 			age: 3,
-// 			tutorId: tutor.id,
-// 		},
-// 	});
-
-// 	console.log('Cachorro criado:', dog);
-
-// 	const photo = await prisma.photo.create({
-// 		data: {
-// 			url: 'https://exemplo.com/foto-do-rex.jpg',
-// 			dogId: dog.id,
-// 		},
-// 	});
-
-// 	console.log('Foto criada:', photo);
-// }
 app.get('/', (req, res) => {
 	res.send('PawPal API funcionando 🐾');
 });
 
-app.post('/users', async (req, res) => {
- try {
-	const {name, email, password, phone, role, dogs} = req.body;
- }
-}
+app.post('/users', async (req: Request, res: Response) => {
+	try {
+		const validated = userSchema.parse(req.body);
+		const emailExists = await prisma.user.findUnique({
+			where: { email: validated.email },
+		});
+
+		if (emailExists) {
+			return res
+				.status(400)
+				.json({ error: 'Não foi possível criar o usuário' });
+		}
+
+		const hashedPassword = await bcrypt.hash(validated.password, 10);
+
+		const user = await prisma.user.create({
+			data: {
+				...validated,
+				password: hashedPassword,
+			},
+		});
+
+		const { password, ...userWithoutPassword } = user;
+
+		return res.status(201).json(userWithoutPassword);
+	} catch (err: any) {
+		if (err.name === 'ZodError') {
+			return res.status(400).json({ error: err.errors });
+		}
+
+		console.error(err);
+		return res
+			.status(500)
+			.json({ error: 'Erro interno ao criar usuário. Tente novamente.' });
+	}
+});
+
+app.get('/users', async (req: Request, res: Response) => {
+	try {
+		const { role } = req.query;
+
+		const users = await prisma.user.findMany({
+			where: role ? { role: role as Role } : {},
+			select: {
+				id: true,
+				name: true,
+				email: true,
+				phone: true,
+				role: true,
+				dogs: true,
+			},
+		});
+
+		return res.status(200).json(users);
+	} catch (err) {
+		console.error(err);
+		return res.status(500).json({ error: 'Erro ao buscar usuários' });
+	}
+});
 
 app.listen(PORT, () => {
 	console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
---
+++ backend/src/validators/userSchema.ts
@@ -0,0 +1,13 @@
+import { z } from 'zod';
+
+export const userSchema = z.object({
+	name: z
+		.string()
+		.min(3, { message: 'Nome precisa ter ao menos 3 caracteres.' }),
+	email: z.string().email({ message: 'E-mail inválido' }),
+	password: z
+		.string()
+		.min(6, { message: 'senha precisa ter ao menos 6 caracteres' }),
+	phone: z.string().min(11, { message: 'Número de telefone inválido' }),
+	role: z.enum(['TUTOR', 'MONITOR', 'GERENTE']),
+});
